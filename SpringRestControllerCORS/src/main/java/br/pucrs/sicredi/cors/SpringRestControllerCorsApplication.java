4ab707bdf789a46afa4671ce9b6000071bab0d9f
Michael da Costa Móra <michael.c.mora@gmail.com>
2021-11-24 02:04:47 -0200

    Commit Inicial

---
+++ SpringRestControllerCORS/src/main/java/br/pucrs/sicredi/cors/SpringRestControllerCorsApplication.java
@@ -0,0 +1,25 @@
+package br.pucrs.sicredi.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SpringRestControllerCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestControllerCorsApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+}
---
+++ SpringRestControllerCORS/src/test/java/br/pucrs/sicredi/cors/SpringRestControllerCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.sicredi.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestControllerCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
